420eeb0c951de87e9e545e6ef1a077d780d98dbf
周文 <chowenzhou@gmail.com>
2017-02-06 16:12:58 +0800

    update

--- library/src/main/java/com/kit/cn/library/notify/IMessageAble.java
+++
@@ -1,9 +0,0 @@
-package com.kit.cn.library.notify;
-
-/**
- * Created by zhouwen on 2016/11/15.
- * Description: msg callback
- */
-public interface IMessageAble {
-    void onMessage(Message message);
-}
